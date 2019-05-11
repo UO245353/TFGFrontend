404d23a8a32ff776eb7b22a237c77cff4290c19f
Daniel Alvarez Vega <wave10496@gmail.com>
2019-05-12 00:40:40 +0200

    add icons + new laYOUT

--- src/views/AdminList.vue
+++ src/views/AdminList.vue
@@ -8,6 +8,14 @@
       </b-col>
     </b-row>
     <b-row>
+      <b-col cols="10">
+        <p class="text-left" style="padding-left: 1rem;" v-on:click="addUser()">Listado de Administradores</p>
+      </b-col>
+      <b-col cols="2">
+        <p class="fas fa-user-plus show-hand-pointer"></p>
+      </b-col>
+    </b-row>
+    <b-row>
       <b-col cols="12">
         <div>
           <b-table
@@ -15,17 +23,23 @@
           hover
           responsive
           :busy="isListNotLoaded"
-          :items="adminList">
-          <div slot="table-busy" class="text-center text-danger my-2">
+          :items="adminList"
+          :fields="fields">
+          <div slot="table-busy" class="text-center text-info my-2">
             <b-spinner class="align-middle"></b-spinner>
             <strong>Loading...</strong>
           </div>
+          <template slot="Editar" slot-scope="data">
+            <i class="fas fa-user-edit show-hand-pointer" v-on:click="editUser(data.item._id)"></i>
+          </template>
+          <template slot="Borrar" slot-scope="data">
+            <i class="fas fa-user-times show-hand-pointer" v-on:click="removeUser(data.item._id)"></i>
+          </template>
         </b-table>
       </div>
     </b-col>
   </b-row>
   <br>
-
 </div>
 </template>
 
@@ -40,6 +54,13 @@ export default {
       slide: 0,
       sliding: null,
       isListNotLoaded: true,
+      fields: [
+        { key: '_id', label: 'Id' },
+        { key: 'name', label: 'Nombre' },
+        { key: 'email', label: 'Correo Electronico' },
+        'Editar',
+        'Borrar'
+      ],
       items: [
         {
           text: 'Administradores',
@@ -53,6 +74,17 @@ export default {
       ]
     }
   },
+  methods: {
+    editUser(id) {
+      console.log(id);
+    },
+    addUser(id) {
+      console.log();
+    },
+    removeUser(id) {
+      console.log(id);
+    }
+  },
   created() {
 
     return axios({
@@ -64,7 +96,7 @@ export default {
     })
     .then(resp => {
 
-      this.adminList = resp.data.obj;
+      this.adminList = resp.data.obj
 
       this.isListNotLoaded = false;
 
