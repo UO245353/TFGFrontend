0e3d1a1658a86326dc874bc14664de4852729a8e
Ubuntu <ubuntu@ip-172-31-24-83.us-east-2.compute.internal>
2019-06-17 07:51:17 +0000

    config changes

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ var CONFIG = function(isLocal, urls){
 export default new CONFIG(
   false,
   {
-    BACKEND_URL_BASE: 'https://gestion-ti.wave-uniovi.es:23456',
+    BACKEND_URL_BASE: 'https://gestion-ti.wave-uniovi.es:12345',
     BACKEND_URL_BASE_LOCAL: 'http://localhost:23456',
   }
 );
